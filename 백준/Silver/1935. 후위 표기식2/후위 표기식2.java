7ec51928c15ea3c95dd266d20c53401ea201eb95
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2023-08-10 23:08:01 +0900

    Update 후위 표기식2.java

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -1,47 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.Stack;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String prefix = br.readLine();
 
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int [] arr = new int[T+1];
+        String s = sc.next();
+
+        for(int i = 0; i<T; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        Stack<Double> operand = new Stack<>();
-        int len = prefix.length();
-        for(int i = 0; i < len; i++) {
-            char ch = prefix.charAt(i);
-            if('A' <= ch && ch <= 'Z') {  // operand
-                double d = arr[ch - 'A'];
-                operand.push(d);
-            }else {  // operator
-                double d1 = operand.pop();
-                double d2 = operand.pop();
-                double d3 = 0.0;
-                switch(ch) {
-                    case '+' :
-                        d3 = d2 + d1;
-                        break;
-                    case '-' :
-                        d3 = d2 - d1;
-                        break;
-                    case '*' :
-                        d3 = d2 * d1;
-                        break;
-                    case '/' :
-                        d3 = d2 / d1;
-                        break;
-                }
-                operand.push(d3);
+        //내가 멍청했다. A,B,... 그냥 문제에서 주는 거였는데 굳이 코드에 표현하려함
+        //어차피 string으로 받고 배열 따로 선언해서 풀어도 되는건데 ㅇㅇ
+
+        Stack<Double> st = new Stack<>();
+
+        for(int i = 0; i<s.length(); i++) {
+
+            char c = s.charAt(i);
+            double a1 = 0;
+            double a2 = 0;
+            double a3 = 0;
+
+            if (c == '+') {
+                a1 = st.pop();
+                a2 = st.pop();
+                a3 = a2 + a1;
+                st.push(a3);
+            } else if (c == '-') {
+                a1 = st.pop();
+                a2 = st.pop();
+                a3 = a2 - a1;
+                st.push(a3);
+            } else if (c == '*') {
+                a1 = st.pop();
+                a2 = st.pop();
+                a3 = a2 * a1;
+                st.push(a3);
+            } else if (c == '/') {
+                a1 = st.pop();
+                a2 = st.pop();
+                a3 = a2 / a1;
+                st.push(a3);
+            } else {
+                int index = c - 'A';
+                st.add((double) arr[index]);
             }
         }
-        System.out.printf("%.2f", operand.pop());
+        double result = st.pop();
+        System.out.printf("%.2f", result);
+
     }
+
+
+
+
+
 }
+
+
