f5c8180812c7eaf13a644f294ba8cf54c51ab614
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2023-08-31 18:32:43 +0900

    [Silver IV] Title: 설탕 배달, Time: 228 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int cnt = 0;
+
+        if(N == 4 || N==7){
+            cnt = -1;
+        }
+
+        else if(N%5 == 0){
+            cnt = N/5;
+        }
+
+        else if(N%5 == 1 || N%5 == 3){
+            cnt = (N/5) + 1;
+        }
+
+        else if(N%5 == 2 || N%5 == 4){
+            cnt = (N/5) + 2;
+        }
+
+
+        System.out.println(cnt);
+
+    }
+}
