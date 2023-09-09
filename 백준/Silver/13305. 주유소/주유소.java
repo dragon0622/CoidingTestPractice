9eb3d3db96f92cf7eeea03d097e03bc21562efb6
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2023-09-09 20:13:31 +0900

    [Silver III] Title: 주유소, Time: 1056 ms, Memory: 170640 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc =new Scanner(System.in);
+        int numOfCity = sc.nextInt();
+        int []dis = new int[numOfCity - 1];
+        int []fuel = new int[numOfCity];
+
+        for (int i = 0; i < numOfCity-1; i++) {
+            dis[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < numOfCity; i++) {
+            fuel[i] = sc.nextInt();
+        }
+
+        int charger = fuel[0];
+        int cost = 0;
+        
+        for (int i = 0; i < numOfCity-1; i++) {
+            if (fuel[i]>fuel[i+1]) {
+                cost += charger * dis[i];
+                charger = fuel[i+1];
+            }
+            else{
+                cost += charger * dis[i];
+            }
+        }
+
+        System.out.println(cost);
+        sc.close();
+    }
+}
