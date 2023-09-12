f7ac99451a1ac4b3402fa04b64680f93e9e54a2b
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2023-09-12 20:57:42 +0900

    [Silver V] Title: 돌 게임, Time: 212 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stubsc.close();
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if (N % 2 == 0){
+            System.out.println("CY");
+        }
+        else{
+            System.out.println("SK");
+        }
+
+
+    }
+
+}
+
