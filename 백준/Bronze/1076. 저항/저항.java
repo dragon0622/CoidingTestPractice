eeb7f9c9b588fb5fe7107bce121cbf47b9c9b3db
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2023-09-23 00:26:39 +0900

    [Bronze II] Title: 저항, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.java
+++ 백준/Bronze/1076. 저항/저항.java
@@ -1,27 +1,44 @@
+import java.util.Scanner;
 import java.util.*;
+import java.math.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[] color = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
-        int[] val = {0,1,2,3,4,5,6,7,8,9};
-        int[] mul = {1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
-        String[] rst = new String[3];
-        int[] value = new int[3];
-        long[] multi = new long[3];
+        
         Scanner sc = new Scanner(System.in);
-        for(int i = 0; i < 3; i++) {
-            String word = sc.next();
-            rst[i] = word;
-        }
-        for(int i = 0; i < color.length; i++) {
-            for(int j = 0; j < rst.length; j++) {
-                if(color[i].equals(rst[j])) {
-                    value[j] = val[i];
-                    multi[j] = mul[i];
-                }
+
+
+        String col = sc.next();
+        String col2 = sc.next();
+        String col3 = sc.next();
+
+        String[] arr = {"black","brown","red","orange",
+        "yellow","green","blue","violet","grey","white"};
+
+
+        long a = 0;
+        long b = 0;
+        long c = 0;
+        long result = 0;
+    
+
+        for (int i = 0; i < arr.length; i++) {
+            if (col.equals(arr[i])) {
+                a = i;
+            }
+
+            if (col2.equals(arr[i])) {
+                b= i;
+            }
+
+            if (col3.equals(arr[i])) {
+                c = (int)Math.pow(10,i);
             }
         }
-        long rst1 = ((value[0] * 10) + value[1]) * multi[2];
-        System.out.println(rst1);
+
+        result = ((a*10)+b)*c;
+
+        System.out.println(result);
+        
     }
 }
