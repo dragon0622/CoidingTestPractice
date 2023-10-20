6d1d8c88689753752b0713202f4b5e0c13a67fc4
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2023-10-20 23:40:27 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1664 ms, Memory: 127176 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(String.valueOf(arr[i]));
+            bw.write("\n");
+        }
+        
+        br.close();
+        bw.close();
+
+    }
+}
