36590a44d89b5cf6d325cbd68cad8ab7b92a5887
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2023-10-22 23:22:28 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        int cnt = 0;
+        while (n >=2 && m>=1 && m+n >= 3+k) {
+            n = n-2;
+            m = m-1;
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
