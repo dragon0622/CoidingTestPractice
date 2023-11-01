8f6568cd7704ca377740a87b79209a09aff9413b
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2023-11-01 23:24:02 +0900

    [Bronze II] Title: 막대기, Time: 276 ms, Memory: 25568 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
 
         Stack<Integer> st = new Stack<>();
 
         for (int i = 0; i < n; i++) {
-            st.push(sc.nextInt());
+            st.push(Integer.parseInt(br.readLine()));
         }
 
         //비교대상인 마지막 값 
