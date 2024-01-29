e5938a861e2d7538734fa261108bb50ef97a4f88
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2024-01-29 16:14:22 +0900

    [level 0] Title: n의 배수, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int num, int n) {
+        int answer = 0;
+            
+        
+        if(n == 0){
+         answer = 0;   
+        }
+        else if(num % n == 0){
+            answer = 1;
+        }
+        else{
+            answer = 0;
+        }
+            
+        return answer;
+    }
+}
