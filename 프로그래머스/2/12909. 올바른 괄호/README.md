0169f6adf8491ca55a83e44458eb304e19a01d13
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2024-02-01 17:52:59 +0900

    [level 2] Title: 올바른 괄호, Time: 17.04 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,6 +1,6 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 17:52:2
+2024년 2월 4일 17:52:59
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,19 +1,39 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Stack;
 
-class Solution {
+public class Solution {
 
-    boolean solution(String s) {
-        Stack<Character> st = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                st.push('(');
-            } else if (s.charAt(i) == ')') {
-                if (st.isEmpty()) {
-                    return false;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        boolean solution(String s)
+        {
+            for (int i = 0; i < s.length(); i++) {
+                if(s.charAt(i) == '('){
+                    st.push(s.charAt(i));
                 }
-                st.pop();
+                else{
+                    if (st.size() == 0){
+                        return false;
+                    }
+                    st.pop();
+                }
             }
+            if (st.size() == 1){
+            return true;
+            }
+            else{
+            return false;
+            }
+        
+
+
         }
-        return st.isEmpty();
+
+        Stack<Character> st = new Stack<>();
+
+        
+
     }
 }
