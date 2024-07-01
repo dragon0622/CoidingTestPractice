9aec9b52b16b8c4fb0e379d7a17575bda91eadf9
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2024-07-01 14:44:23 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 172 ms, Memory: 17592 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -10,22 +10,35 @@ public class Main {
 
         int cnt = 0;
 
+        toCharacterArr(characters, word);
+
+        cnt = getCnt(characters, cnt);
+
+        makeResult(cnt, characters);
+
+    }
+
+    private static void toCharacterArr(Character[] characters, String word) {
         for (int i = 0; i < characters.length; i++) {
             characters[i] = word.charAt(i);
         }
+    }
 
+    private static int getCnt(Character[] characters, int cnt) {
         for (int i = 0; i < characters.length/2; i++) {
             if (characters[i] == characters[characters.length-1-i]){
                 cnt++;
             }
         }
+        return cnt;
+    }
 
+    private static void makeResult(int cnt, Character[] characters) {
         if (cnt == characters.length/2){
             System.out.println("1");
         }
         else {
             System.out.println("0");
         }
-
     }
 }
