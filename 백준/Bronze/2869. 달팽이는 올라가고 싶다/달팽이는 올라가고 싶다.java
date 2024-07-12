1b619b84d6efb23e2bc02eadc3e88b07f45f1709
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2024-07-12 10:26:49 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int day = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0){
+            day++;
+        }
+        System.out.println(day);
+    }
+}
