20fc8da768c72e3f8d884a9578735993b723eef2
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2024-07-15 15:16:22 +0900

    [Bronze III] Title: FBI, Time: 188 ms, Memory: 18124 KB -BaekjoonHub

---
+++ 백준/Bronze/2857. FBI/FBI.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = new String[5];
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.next();
+            if (arr[i].contains("FBI")){
+                sb.append(i+1 + " ");
+            }
+        }
+
+        if(sb.length() == 0 ){
+            sb.append("HE GOT AWAY!");
+        }
+
+        System.out.println(sb);
+
+    }
+}
