fcfe225d9bcef7c3e5fb3206176e0230f6a45d1a
dragon0622 <141225643+dragon0622@users.noreply.github.com>
2024-07-28 23:42:31 +0900

    [Bronze II] Title: 하얀 칸, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.java
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -1,22 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int cnt = 0;
 
         for (int i = 0; i < 8; i++) {
-            String line = sc.next();
+            String line = br.readLine();
             for (int j = 0; j < 8; j++) {
                 char character = line.charAt(j);
                 if (character == 'F'){
-                    if ((i+j)%2 == 0){
-                        cnt++;
-                    }
+                    cnt = getCnt(i, j, cnt);
                 }
             }
         }
         System.out.println(cnt);
+    }
+
+    private static int getCnt(int i, int j, int cnt) {
+        if ((i + j)%2 == 0){
+            cnt++;
+        }
+        return cnt;
     }
 }
