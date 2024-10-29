fd25c1fb6d4120ca85b992ab83995fe5b11f4670
김정용 <141225643+dragon0622@users.noreply.github.com>
2024-10-29 18:02:31 +0900

    [Silver V] Title: 국회의원 선거, Time: 180 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -2,13 +2,10 @@ import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
+        
         int n = sc.nextInt();
-
         int[] arr = new int[n];
 
         for (int i = 0; i < n; i++) {
@@ -16,42 +13,26 @@ public class Main {
         }
 
         int start = arr[0];
-
-
         int maxIdx = whoIsTop(arr);
 
-        while(true){
-            arr = operation(maxIdx, arr);
+        while (maxIdx != 0) {
+            arr[0]++;
+            arr[maxIdx]--;
             maxIdx = whoIsTop(arr);
-
-            if (maxIdx == 0){
-                break;
-            }
         }
 
         int answer = start - arr[0];
-
         System.out.println(Math.abs(answer));
-
         sc.close();
-    }
-
-    private static int[] operation(int i1, int[] arr) {
-        arr[0] ++;
-        arr[i1]--;
-        return arr;
     }
 
     private static int whoIsTop(int[] arr) {
         int maxIdx = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[maxIdx] <= arr[i]){
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] >= arr[maxIdx]) {
                 maxIdx = i;
             }
         }
-
-
         return maxIdx;
     }
 }
