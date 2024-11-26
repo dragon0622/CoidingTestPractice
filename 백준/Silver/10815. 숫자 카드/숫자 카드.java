d7ee692499e141b7f287b639b698bb2d91fe5fbc
김정용 <141225643+dragon0622@users.noreply.github.com>
2024-11-26 15:35:01 +0900

    [Silver V] Title: 숫자 카드, Time: 848 ms, Memory: 135504 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,25 +1,32 @@
-
+import java.io.*;
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         Set<Integer> set = new HashSet<>();
+
         for (int i = 0; i < n; i++) {
-            set.add(sc.nextInt());
+            set.add(Integer.parseInt(st.nextToken()));
         }
 
-        int m = sc.nextInt();
+        int m = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
 
+        st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < m; i++) {
-            int num = sc.nextInt();
+            int num = Integer.parseInt(st.nextToken());
             if (set.contains(num)){
                 sb.append("1 ");
             }
@@ -28,7 +35,9 @@ public class Main {
             }
         }
 
-        System.out.println(sb);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
 
 }
 }
